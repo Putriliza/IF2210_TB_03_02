ca4593024d2eaa110d5abccceab8e3dc313dccef
pandora-1 <michaelkahari.mk@gmail.com>
2022-04-07 23:51:44 +0700

    Adding mana

---
+++ Mana/mana.java
@@ -0,0 +1,52 @@
+public class Mana {
+    private int jumlah;
+    private int giliran;
+
+    public Mana() {
+        this.jumlah = 1;
+        this.giliran = 1;
+    }
+
+    public Mana(int jumlah, int giliran) {
+        this.jumlah = jumlah;
+        this.giliran = giliran;
+    }
+
+    public int getJumlah() {
+        return jumlah;
+    }
+
+    public void setJumlah(int jumlah) {
+        this.jumlah = jumlah;
+    }
+
+    public int getGiliran() {
+        return giliran;
+    }
+
+    public void setGiliran(int giliran) {
+        this.giliran = giliran;
+        if(this.giliran > 10) {
+            this.jumlah = 10;
+        } else {
+            this.jumlah = this.giliran;
+        }
+    }
+
+    public void tambahGiliran(int giliran) {
+        this.giliran += giliran;
+        if(this.giliran > 10) {
+            this.jumlah = 10;
+        } else {
+            this.jumlah = this.giliran;
+        }
+    }
+
+    public int getMana() {
+        if(this.giliran > 10) {
+            return 10;
+        } else {
+            return this.jumlah;
+        }
+    }
+}
