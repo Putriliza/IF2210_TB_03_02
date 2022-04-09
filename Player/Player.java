04e4edaf04383e900836fc5245b9248238d32d2c
adellinekania <adellinekaniasetiyawan@gmail.com>
2022-04-09 14:29:38 +0700

    coba skeleton player

---
+++ Player/Player.java
@@ -0,0 +1,76 @@
+import java.util.ArrayList;
+
+public class Player {
+    // Atribut
+    private String name;
+    private int healtPoints;
+    private Mana mana;
+    private ArrayList <Kartu> deck;
+    private ArrayList <Kartu> hand;
+
+    // Konstruktor
+    public Player(String name) {
+        this.name = name;
+        this.healtPoints = 80;
+        this.mana = new Mana(1,1);
+
+        // Inisialisasi kartu di deck
+        this.deck = new ArrayList<Kartu>();
+        // TO DO: Nanti kalo kartu karakter sama spell dah ada, inisiasi dengan jumlah yang sama
+        for (int i = 0; i < 60; i++) {
+            this.deck.add(new Kartu("Kartu " + i, "Deskripsi " + i, "Tipe " + i));
+        }
+
+        // Inisiasi kartu di hand
+        this.hand = new ArrayList<Kartu>();
+    }
+
+    // Getter & Setter
+    public void setHealthPoints(int healthPoints) {
+        if (healthPoints >= 0 && healthPoints <= 80) {
+            this.healtPoints = healthPoints;
+        }
+    }
+
+    // Ambil kartu dari deck
+    public void ambilKartu(){
+        // TO DO:
+        // Cek apakah deck kosong
+        // Cek apakah mana cukup untuk mengambil kartu
+        // Cek apakah kartu di hand sudah 5, kalo iya buang dulu yang di hand
+        // Jika sudah memenuhi semua, maka ambil kartu dari deck secara random
+        // Pindahkan kartu dari deck ke hand (kurangin yang di deck, tambahin di hand)
+        // Kurangi mana
+    }
+
+    public void descKartu(){
+        // TO DO:
+        // Tampilin deskripsi kartu yang ada di deck atau hand
+    }
+
+    public void keluarkanKartu(){
+        // TO DO:
+        // Keluarkan kartu yang ada di hand
+        // Masukin ke dalam board, cek apakah di board masih ada slot atau ga
+    }
+
+    public void keluarkanMana(){
+        // TO DO:
+        // Keluarkan mana yang dipunya
+        // Pilih kartu karakter di board yang mau di pakaiin mana
+        // Kartu karakter nambah exp nya
+    }
+
+    public static void main(String[] args) {
+        Player player = new Player("Player");
+        // player.setHealtPoints(10);
+        // player.setMana(new Mana(1, 1));
+        // player.setDeck(new ArrayList<Kartu>());
+        // player.setHand(new ArrayList<Kartu>());
+        // player.getMana().setGiliran(1);
+        // player.getMana().setJumlah(1);
+        // System.out.println(player.getMana().getMana());
+        // player.getMana().tambahGiliran(1);
+        // System.out.println(player.getMana().getMana());
+    }
+}
