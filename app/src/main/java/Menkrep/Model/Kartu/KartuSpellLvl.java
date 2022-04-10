a440093f6b0b6b97111d7249182dcb78bc3edfe1
putriliza <putriliza05@gmail.com>
2022-04-10 21:30:07 +0700

    spell level potion (masih kurang)

---
+++ app/src/main/java/Menkrep/Model/Kartu/KartuSpellLvl.java
@@ -0,0 +1,30 @@
+package Menkrep.Model.Kartu;
+
+import Menkrep.Model.Mana.Mana;
+
+public class KartuSpellLvl extends KartuSpell {
+    KartuSpellLvl() {
+        super("-", "-", "LVL", new Mana(), 0);
+    }
+
+    KartuSpellLvl(String nama, String deskripsi, Mana mana, int durasi) {
+        super(nama, deskripsi, "LVL", mana, durasi);
+    }
+
+    // Meningkatkan/menurunkan level dari sebuah karakter sebanyak 1
+    // serta mereset exp (membuat exp menjadi 0). Batas minimal level tetap 1 dan maksimal tetap 10.
+    public void lvl(KartuKarakter karakter, boolean up) {
+        int level = karakter.getLevel();
+        if (up) {
+            if (level < 10) {
+                karakter.setLevel(level + 1);
+                karakter.setExp(0);
+            }
+        } else {
+            if (level > 1) {
+                karakter.setLevel(level - 1);
+                karakter.setExp(0);
+            }
+        }
+    }
+}
---
+++ app/src/main/java/Menkrep/Model/Kartu/KartuSpellPotion.java
@@ -0,0 +1,30 @@
+package Menkrep.Model.Kartu;
+
+import Menkrep.Model.Mana.Mana;
+
+public class KartuSpellPotion extends KartuSpell {
+    KartuSpellPotion() {
+        super("-", "-", "POTION", new Mana(), 0);
+    }
+
+    KartuSpellPotion(String nama, String deskripsi, Mana mana, int durasi) {
+        super(nama, deskripsi, "POTION", mana, durasi);
+    }
+
+    /* Meningkatkan atribut attack dan / atau health.
+    Penambahan ini dapat bernilai 0 bahkan negatif.
+    Penambahan health dilakukan secara bertumpuk, ????
+    sehingga serangan musuh akan mengambil health dari efek potion terakhir terlebih dahulu
+    sebelum health dari karakter atau efek potion sebelumnya.
+    Pengurangan health dapat membunuh karakter.
+    Pengurangan attack tidak akan membuat attack karakter kurang dari 0. */
+    public void potion(KartuKarakter karakter, int attack, int health) {
+        int newAttack = karakter.getAttack() + attack;
+        int newHealth = karakter.getHealth() + health;
+        if (newAttack < 0) {
+            newAttack = 0;
+        }
+        karakter.setAttack(newAttack);
+        karakter.setHealth(newHealth);
+    }
+}
