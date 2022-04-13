a78f592d30ad777a42be7309cde2729af9317147
vito <vitosgh@gmail.com>
2022-04-13 20:44:13 +0700

    Fix CSV import directory

--- app/src/main/java/Menkrep/App.java
+++ app/src/main/java/Menkrep/App.java
@@ -3,10 +3,10 @@
  */
 package Menkrep;
 
-//import Menkrep.Model.Reference.Reference;
+import Menkrep.Model.Reference.Reference;
 //
-//import java.io.IOException;
-//import java.net.URISyntaxException;
+import java.io.IOException;
+import java.net.URISyntaxException;
 
 
 import javax.swing.*;
@@ -22,12 +22,12 @@ public class App{
     }
 
     public static void main(String[] args){
-//        try {
-//            Reference kartuReference = new Reference();
-//            System.out.println(kartuReference.getKarakter());
-//        } catch (IOException | URISyntaxException e) {
-//            System.out.println("Error");
-//        }
+        try {
+            Reference kartuReference = new Reference();
+            System.out.println(kartuReference.getKarakter());
+        } catch (IOException e) {
+            System.out.println("Error: directory not found");
+        }
         System.out.println("Hello world");
 
         JFrame frame = new JFrame("Menkrep");
--- app/src/main/java/Menkrep/Model/Reference/Reference.java
+++ app/src/main/java/Menkrep/Model/Reference/Reference.java
@@ -8,32 +8,35 @@ import java.net.URISyntaxException;
 import java.util.List;
 
 public class Reference {
-    private static final String CHARACTER_CSV_FILE_PATH = "card/data/character.csv";
-    private static final String MORPH_CSV_FILE_PATH = "card/data/spell_morph.csv";
-    private static final String PTN_CSV_FILE_PATH = "card/data/spell_ptn.csv";
-    private static final String SWAP_CSV_FILE_PATH = "card/data/spell_swap.csv";
+    private static final String CHARACTER_CSV_FILE_PATH = "data/character.csv";
+    private static final String MORPH_CSV_FILE_PATH = "data/spell_morph.csv";
+    private static final String PTN_CSV_FILE_PATH = "data/spell_ptn.csv";
+    private static final String SWAP_CSV_FILE_PATH = "data/spell_swap.csv";
     private final List<String[]> karakter;
     private final List<String[]> morph;
     private final List<String[]> ptn;
     private final List<String[]> swap;
 
-    public Reference() throws IOException, URISyntaxException {
-        File characterCSVFile = new File(getClass().getResource(CHARACTER_CSV_FILE_PATH).toURI());
+    public Reference() throws IOException {
+        String cwd = System.getProperty("user.dir");
+        String cardDirectory = "\\app\\src\\main\\resources\\Menkrep\\card\\";
+
+        File characterCSVFile = new File(cwd + cardDirectory + CHARACTER_CSV_FILE_PATH);
         CSVReader characterReader = new CSVReader(characterCSVFile, "\t");
         characterReader.setSkipHeader(true);
         this.karakter = characterReader.read();
 
-        File morphCSVFile = new File(getClass().getResource(MORPH_CSV_FILE_PATH).toURI());
+        File morphCSVFile = new File(cwd + cardDirectory + MORPH_CSV_FILE_PATH);
         CSVReader morphReader = new CSVReader(morphCSVFile, "\t");
         morphReader.setSkipHeader(true);
         this.morph = morphReader.read();
-
-        File ptnCSVFile = new File(getClass().getResource(PTN_CSV_FILE_PATH).toURI());
+//
+        File ptnCSVFile = new File(cwd + cardDirectory + PTN_CSV_FILE_PATH);
         CSVReader ptnReader = new CSVReader(ptnCSVFile, "\t");
         ptnReader.setSkipHeader(true);
         this.ptn = ptnReader.read();
 
-        File swapCSVFile = new File(getClass().getResource(SWAP_CSV_FILE_PATH).toURI());
+        File swapCSVFile = new File(cwd + cardDirectory + SWAP_CSV_FILE_PATH);
         CSVReader swapReader = new CSVReader(swapCSVFile, "\t");
         swapReader.setSkipHeader(true);
         this.swap = swapReader.read();
