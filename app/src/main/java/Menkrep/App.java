098db3fb29d879501477ce5aaa05fc391db63495
vito <vitosgh@gmail.com>
2022-04-24 00:27:10 +0700

    Add GUI template

--- app/src/main/java/Menkrep/App.java
+++ app/src/main/java/Menkrep/App.java
@@ -5,35 +5,55 @@ package Menkrep;
 
 import Menkrep.Model.Reference.Reference;
 //
-import java.io.IOException;
-import java.net.URISyntaxException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import Menkrep.GUI.FXMLController;
+
+import static javafx.application.Application.launch;
 
 
-import javax.swing.*;
-import java.awt.*;
-
-public class App{
-
-    private JPanel mainPanel;
-    private JButton button1;
-
-    public String getGreeting(){
+public class App extends Application {
+    public String getGreeting() {
         return "Hello world";
     }
 
-    public static void main(String[] args){
-        try {
-            Reference kartuReference = new Reference();
-            System.out.println(kartuReference.getKarakter());
-        } catch (IOException e) {
-            System.out.println("Error: directory not found");
-        }
-        System.out.println("Hello world");
+//    public void start(Stage stage) {
+//        Text text = new Text();
+//        text.setText("Loading...");
+//        text.setX(50);
+//        text.setY(50);
+//
+//        Group root = new Group();
+//        root.getChildren().add(text);
+//
+//        Scene scene = new Scene(root, 1280, 720);
+//
+//        stage.setTitle("Menkrep");
+//        stage.setScene(scene);
+//        stage.show();
+//
+//        text.setText("Menkrep card battle");
+//    }
 
-        JFrame frame = new JFrame("Menkrep");
-        frame.setContentPane(new JPanel());
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
+    @Override
+    public void start(Stage stage) throws Exception {
+        System.out.println(getClass().getResource("scene.fxml"));
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("scene.fxml"));
+        AnchorPane root = loader.load();
+        Scene scene = new Scene(root);
+//        scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
+
+        stage.setTitle("Menkrep");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
